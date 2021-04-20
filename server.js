cb3f12cae6bceff23ca08a56f2be219da512d53f
Stefan Sjöman <stefan.sjoman@hotmail.com>
2021-04-20 09:49:17 +0200

    added basic router, imported path and cors

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 1337;
 
-app.get('/', (req, res) => {
-	res.send('Hamsters coming soon!')
-});
+const cors = require('cors');
+const path = require('path');
+
+const hamsters = require('./routes/hamsters.js');
+
+app.use('/hamsters', hamsters);
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
