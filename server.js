a68d9065caeada724b3c3f0b75ffbdfea43f962e
Stefan Sjöman <stefan.sjoman@hotmail.com>
2021-04-20 10:31:27 +0200

    added middleware

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// import packages & server config
 const express = require('express');
 const app = express();
 const PORT = 1337;
@@ -7,8 +8,19 @@ const path = require('path');
 
 const hamsters = require('./routes/hamsters.js');
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.url}`, req.params);
+	next();
+});
+
+// routes
 app.use('/hamsters', hamsters);
 
+// start server
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
 });
